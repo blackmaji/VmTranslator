9906afed8610431dd566d97663f824cd4a098dd8
blackmaji <victor.pereirah@gmail.com>
2023-11-21 11:18:39 -0300

    feita adição da classe Main.
    Adicionada classe Main para abertura e leitura do arquivo .vm, criada função fromFile e main.

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class Main {
+    private static String fromFile(File file) {        
+
+        byte[] bytes;
+        try {
+            bytes = Files.readAllBytes(file.toPath());
+            String textoDoArquivo = new String(bytes, "UTF-8");
+            return textoDoArquivo;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
+    } 
+
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.err.println("Please provide a single file path argument.");
+            System.exit(1);
+        }
+
+    }
+}
