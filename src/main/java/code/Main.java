5768dcd5c6daaca0542b6c39edc30aa47a524655
blackmaji <victor.pereirah@gmail.com>
2023-12-03 21:19:39 -0300

    Feitas mudanças na classe Main.
    Implementada mudanças no sistema de leitura de arquivos.

--- src/main/java/code/Main.java
+++ src/main/java/code/Main.java
@@ -1,132 +1,106 @@
-package main.java.code;
-
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
-public class Main {
-    private static String fromFile(File file) {        
-
-        byte[] bytes;
-        try {
-            bytes = Files.readAllBytes(file.toPath());
-            String textoDoArquivo = new String(bytes, "UTF-8");
-            return textoDoArquivo;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return "";
-    } 
-
+/**
+ * Created by danakatz on 10/28/14.
+ */
+public class VMTranslator {
     public static void main(String[] args) {
+        if(args.length > 0) {
+            try {
+                ArrayList<File> files = new ArrayList<File>();
 
-        if (args.length != 1) {
-            System.err.println("Please provide a single file path argument.");
-            System.exit(1);
-        }
+                File input = new File(args[0]);
+                getFiles(input, files);
 
-        File file = new File(args[0]);
+                if(!files.isEmpty()) {
+                    String outputName = input.getName();
+                    if(outputName.indexOf('.') > 0) {
+                        outputName = outputName.substring(0, outputName.indexOf('.'));
+                    } else if(outputName.indexOf('/') > 0) {
+                        outputName = outputName.substring(0, outputName.indexOf('/'));
+                    }
 
-        if (!file.exists()) {   
-            System.err.println("The file doesn't exist.");
-            System.exit(1);
-        }
+                    File output;
+                    if(input.isFile()) {
+                        output = new File(input.getParent(), outputName + ".asm");
+                    } else {
+                        output = new File(input, outputName + ".asm");
+                    }
 
-        if (file.isDirectory()) {
+                    CodeWriter cw = new CodeWriter(output);
 
-            var outputFileName = file.getAbsolutePath() +"/"+ file.getName()+".asm";
-            System.out.println(outputFileName);
-            CodeWriter code = new CodeWriter(outputFileName);
+                    cw.writeInit();
 
-            for (File f : file.listFiles()) {
-                if (f.isFile() && f.getName().endsWith(".vm")) {
+                    for(File f : files) {
+                        String name = f.getName();
+                        name = name.substring(0, name.indexOf('.'));
+                        cw.setFileName(name);
 
-                    var inputFileName = f.getAbsolutePath();
-                    var pos = inputFileName.indexOf('.');
-                    
-                    
-                    System.out.println("compiling " +  inputFileName);
-                    translateFile(f,code);
+                        Parser p = new Parser(f);
+                        while(true) {
+                            if(p.commandType() == 0) {
+                                System.out.println(f + " contains an invalid instruction.");
+                                return;
+                            }
 
+                            if(p.commandType() == Parser.C_ARITHMETIC) {
+                                cw.writeArithmetic(p.arg1());
+                            } else if(p.commandType() == Parser.C_PUSH || p.commandType() == Parser.C_POP) {
+                                cw.writePushPop(p.commandType(), p.arg1(), p.arg2());
+                            } else if(p.commandType() == Parser.C_LABEL) {
+                                cw.writeLabel(p.arg1());
+                            } else if(p.commandType() == Parser.C_GOTO) {
+                                cw.writeGoto(p.arg1());
+                            } else if(p.commandType() == Parser.C_IF) {
+                                cw.writeIf(p.arg1());
+                            } else if(p.commandType() == Parser.C_FUNCTION) {
+                                cw.writeFunction(p.arg1(), p.arg2());
+                            } else if(p.commandType() == Parser.C_CALL) {
+                                cw.writeCall(p.arg1(), p.arg2());
+                            } else if(p.commandType() == Parser.C_RETURN) {
+                                cw.writeReturn();
+                            }
+
+                            if(p.hasMoreCommands()) {
+                                p.advance();
+                            } else break;
+                        }
+                    }
+
+                    cw.close();
+                } else {
+                    System.out.println("No .vm files found.");
                 }
 
+            } catch (Exception e) {
+                System.out.println("Error: " + e.getMessage());
+                e.printStackTrace();
             }
-            code.save();
-        } else if (file.isFile()) {
-            if (!file.getName().endsWith(".vm"))  {
-                System.err.println("Please provide a file name ending with .vm");
-                System.exit(1);
-            } else {
-                var inputFileName = file.getAbsolutePath();
-                var pos = inputFileName.indexOf('.');
-                var outputFileName = inputFileName.substring(0, pos) + ".asm";
-                CodeWriter code = new CodeWriter(outputFileName);
-                System.out.println("compiling " +  inputFileName);
-                translateFile(file,code); 
-                code.save();               
-            }
+        } else {
+            System.out.println("No source entered.");
         }
     }
 
-    private static void translateFile (File file, CodeWriter code) {
-
-        String input = fromFile(file);
-        Parser p = new Parser(input);
-        while (p.hasMoreCommands()) {
-            var command = p.nextCommand();
-            switch (command.type) {
-
-                case ADD:
-                    code.writeArithmeticAdd();
-                    break;
-
-                case SUB:
-                    code.writeArithmeticSub();
-                    break;
-
-                case NEG:
-                    code.writeArithmeticNeg();
-                    break;
-
-                case NOT:
-                    code.writeArithmeticNot();
-                    break;
-                
-                case EQ:
-                    code.writeArithmeticEq();
-                    break;
-
-                case LT:
-                    code.writeArithmeticLt();
-                    break;
-                
-                case GT:
-                    code.writeArithmeticGt();
-                    break;
-                
-                case AND:
-                    code.writeArithmeticAnd();
-                    break;
-   
-                case OR:
-                    code.writeArithmeticOr();
-                    break;
-
-                case PUSH:
-                    code.writePush(command.args.get(0), Integer.parseInt(command.args.get(1)));
-                    break;
-                
-                case POP:
-                    code.writePop(command.args.get(0), Integer.parseInt(command.args.get(1)));
-                    break;
-
-                default:
-                    System.out.println(command.type.toString()+" not implemented");
+    private static void getFiles(File input, ArrayList<File> files) throws FileNotFoundException {
+        if(input.isFile()) {
+            // check for .vm extension before adding to list of files
+            String filename = input.getName();
+            int extension = filename.indexOf('.');
+            if(extension > 0) {
+                String fileExtension = filename.substring(extension + 1);
+                if(fileExtension.equalsIgnoreCase("vm")) {
+                    files.add(input);
+                }
             }
-
-    
-        } 
-       
-
+        } else if(input.isDirectory()) {
+            File[] innerFiles = input.listFiles();
+            for(File f : innerFiles) {
+                getFiles(f, files);
+            }
+        } else {
+            throw new FileNotFoundException("Could not find file or directory.");
+        }
     }
 }
